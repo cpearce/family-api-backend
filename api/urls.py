619a21f312f11e80dadc16f42e2a79ff2a3d88e8
Chris Pearce <chris@pearce.org.nz>
2019-09-17 16:16:08 +1200

    Sort URLs.

--- api/urls.py
+++ api/urls.py
@@ -5,13 +5,13 @@ from rest_framework.authtoken.views import obtain_auth_token
 from api import views
 
 urlpatterns = [
-    path('individuals/', views.ListIndividual.as_view()),
-    path('individuals/<int:pk>/', views.DetailIndividual.as_view()),
-    path('individuals/<int:pk>/verbose', views.verbose_individual_detail),
+    path('account/', views.account_details),
     path('families/', views.ListFamily.as_view()),
     path('families/<int:pk>/', views.DetailFamily.as_view()),
     path('families/of-individual/<int:pk>/', views.list_family_of_individual),
+    path('individuals/', views.ListIndividual.as_view()),
+    path('individuals/<int:pk>/', views.DetailIndividual.as_view()),
+    path('individuals/<int:pk>/verbose', views.verbose_individual_detail),
     path('login/', obtain_auth_token),
     path('logout/', views.logout),
-    path('account/', views.account_details),
 ]
